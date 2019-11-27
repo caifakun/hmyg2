1b17b53755a3df5374b669bfa22722831f8955f7
caifakun <466411507@qq.com>
2019-11-27 14:43:36 +0800

    实现轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,21 @@
+Page({
+	data: {
+		swiperList: [
 
-Page({})
+		]
+	},
+	onLoad() {
+		wx.request({
+			url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+			method: 'GET',
+			responseType: 'text',
+			success: (result) => {
+				// console.log(result.data.message);
+				this.setData({
+					swiperList: result.data.message
+				})
+			}
+		});
+
+	}
+})
