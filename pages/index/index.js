58367baa44a0641ba807da8e795ff7a1d492e7db
caifakun <466411507@qq.com>
2019-11-27 17:20:12 +0800

    导航栏

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,14 @@
 Page({
 	data: {
+		// 轮播图
 		swiperList: [
 
-		]
+		],
+		// 导航
+		navigatorList:[]
 	},
 	onLoad() {
+		// 这里是请求获取轮播图数据
 		wx.request({
 			url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
 			method: 'GET',
@@ -13,6 +17,19 @@ Page({
 				// console.log(result.data.message);
 				this.setData({
 					swiperList: result.data.message
+				})
+			}
+		});
+
+		// 这里是请求获取导航分类数据
+		wx.request({
+			url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+			method: 'GET',
+			responseType: 'text',
+			success: (result) => {
+				// console.log(result.data.message);
+				this.setData({
+					navigatorList: result.data.message
 				})
 			}
 		});
