2a8bd0e9743d11f72bbc13e85a45dcb1cc20958d
caifakun <466411507@qq.com>
2019-11-29 19:17:40 +0800

    渲染分类数据

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,42 @@
 // pages/category/index.js
+import request from "../../utils/request";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 左侧数据
+    leftMenues:[],
+    // 右侧数据
+    rightContent:[]
   },
-
+  // 全局内部的数据 wxml中找不到
+  // 但是js内部需要使用到全局数据
+  Categories:[],
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad() {
+    // 请求获取分类数据
+    request({url:'categories'}).then(result=>{
+      // console.log(result.data.message);
+      // 修改全局数据
+      this.Categories = result.data.message;
+      // 修改 左侧数据
+      this.setData({
+        leftMenues : this.Categories.map(v=>v.cat_name) 
+      })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      // 修改右侧数据
+      this.setData({
+        rightContent:this.Categories[0].children
+      })
+      console.log(this.data.rightContent);
+      
+    })
   }
+
+
 })
