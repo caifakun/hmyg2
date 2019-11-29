8a806c74f16c32bcda0aabd33b7d380cb1e88373
caifakun <466411507@qq.com>
2019-11-29 13:00:02 +0800

    利用promise封装axios请求

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,5 @@
+import request from "../../utils/request"
+
 Page({
 	data: {
 		// 轮播图
@@ -5,45 +7,37 @@ Page({
 
 		],
 		// 导航
-		navigatorList:[],
+		navigatorList: [],
 		// 楼层内容
-		floorList:[]
+		floorList: []
 	},
 	onLoad() {
 		// 这里是请求获取轮播图数据
-		wx.request({
-			url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-			method: 'GET',
-			success: (result) => {
-				// console.log(result.data.message);
-				this.setData({
-					swiperList: result.data.message
-				})
-			}
-		});
+		request({
+			url: 'home/swiperdata'
+		}).then(result => {
+			this.setData({
+				swiperList: result.data.message
+			})
+		})
 
 		// 这里是请求获取导航分类数据
-		wx.request({
-			url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
-			method: 'GET',
-			success: (result) => {
-				// console.log(result.data.message);
-				this.setData({
-					navigatorList: result.data.message
-				})
-			}
-		});
+		request({
+			url: 'home/catitems'
+		}).then(result => {
+			this.setData({
+				navigatorList: result.data.message
+			})
+		})
 
 		// 这里 获取首页 楼层数据
-		wx.request({
-			url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
-			method: 'GET',
-			success: (result) => {
-				console.log(result.data.message);
-				this.setData({
-					floorList: result.data.message
-				})
-			}
-		});
+		request({
+			url: 'home/floordata'
+		}).then(result => {
+			// console.log(result.data.message);
+			this.setData({
+				floorList: result.data.message
+			})
+		})
 	}
 })
