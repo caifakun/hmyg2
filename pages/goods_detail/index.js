9b292fa1cb8e74266635b2d735e13f1b756c1ef9
caifakun <466411507@qq.com>
2019-11-30 20:13:22 +0800

    商品详情页轮播图

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,30 @@
 // pages/goods_detail/index.js
+
+import request from '../../utils/request';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 商品详情数据内容
+    goodsDetail: [] 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.getGoodsDetail(options.goods_id);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 封装获取商品详情的方法
+  getGoodsDetail(goods_id) {
+    request({
+      url: 'goods/detail',
+      data: {
+        goods_id
+      }
+    }).then(result => {
+      this.setData({  
+        goodsDetail : result.data.message
+      })
+    })
   }
 })
