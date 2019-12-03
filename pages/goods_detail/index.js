bf9d2570feac65127cffac37291c21c66fb93a3c
caifakun <466411507@qq.com>
2019-12-03 11:52:25 +0800

    添加购物车成功

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -8,7 +8,7 @@ Page({
    */
   data: {
     // 商品详情数据内容
-    goodsDetail: [] 
+    goodsDetail: {}
   },
 
   onLoad: function (options) {
@@ -22,24 +22,44 @@ Page({
         goods_id
       }
     }).then(result => {
-      this.setData({  
-        goodsDetail : result.data.message
+      this.setData({
+        goodsDetail: result.data.message
       })
     })
   },
   // 点击图片，进行预览
-  handldePreviewImage(e){
+  handldePreviewImage(e) {
     const current = e.currentTarget.dataset.src;
-    const urls = this.data.goodsDetail.pics.map(v=>v.pics_big)
+    const urls = this.data.goodsDetail.pics.map(v => v.pics_big)
     wx.previewImage({
       current, // 当前显示图片的http链接
       urls // 需要预览的图片http链接列表
     })
   },
   // 加入购物车
-  addCart(){
-    // console.log(this.data.goodsDetail);
-    const {goodsDetail} = this.data;
-    wx.setStorageSync('cartsList',{goodsDetail} );  
+  addCart() {
+    // 从本地存储中取出来
+    const carts = wx.getStorageSync('carts') || [];
+    console.log(carts);
+
+    // 判断是否已有当前商品，如果存在就只让数量++,否则添加到carts中
+    const index = carts.findIndex(v => v.goods_id == this.data.goodsDetail.goods_id)
+
+    if (index === -1) {
+      carts.unshift({
+        ...this.data.goodsDetail,
+        nums:1
+      })
+    }else{
+      carts[index].nums++
+    }
+
+    wx.setStorageSync('carts', carts);
+
+    wx.showToast({
+      title: '添加成功',
+      icon: 'success',
+      mask:true
+    })
   }
 })
